9b98baa6ad909daad449fa6e49d4de91d2d14dca
knarfrank <fh13286@my.bristol.ac.uk>
2016-12-19 09:14:46 +0800

    Remove key schedule and remove decryption

--- AES_TEST/target.c
+++ AES_TEST/target.c
@@ -86,12 +86,6 @@ void print(char* string, int n) {
 typedef uint8_t state_t[4][4];
 static state_t* state;
 
-// The array that stores the round keys.
-static uint8_t RoundKey[176];
-
-// The Key input to the AES Program
-static const uint8_t* Key;
-
 // The lookup-tables are marked const so they can be placed in read-only storage instead of RAM
 // The numbers below can be computed dynamically trading ROM for RAM - 
 // This can be useful in (embedded) bootloader applications, where ROM is often limited.
@@ -114,82 +108,7 @@ static const uint8_t sbox[256] =   {
   0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
   0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 };
 
-static const uint8_t rsbox[256] =
-{ 0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
-  0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
-  0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
-  0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
-  0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
-  0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
-  0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
-  0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
-  0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
-  0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
-  0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
-  0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
-  0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
-  0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
-  0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
-  0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d };
-
-
-// The round constant word array, Rcon[i], contains the values given by 
-// x to th e power (i-1) being powers of x (x is denoted as {02}) in the field GF(2^8)
-// Note that i starts at 1, not 0).
-static const uint8_t Rcon[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
-
-
-// This function produces Nb(Nr+1) round keys. The round keys are used in each round to decrypt the states. 
-static void KeyExpansion(void) {
-  uint32_t i, k;
-  uint8_t tempa[4]; // Used for the column/row operations
-  
-  // The first round key is the key itself.
-  for(i = 0; i < 4; ++i) {
-    RoundKey[(i * 4) + 0] = Key[(i * 4) + 0];
-    RoundKey[(i * 4) + 1] = Key[(i * 4) + 1];
-    RoundKey[(i * 4) + 2] = Key[(i * 4) + 2];
-    RoundKey[(i * 4) + 3] = Key[(i * 4) + 3];
-  }
-
-  // All other round keys are found from the previous round keys.
-  for(; i < 44; ++i) {
-    tempa[0] = RoundKey[(i-1) * 4 + 0];
-    tempa[1] = RoundKey[(i-1) * 4 + 1];
-    tempa[2] = RoundKey[(i-1) * 4 + 2];
-    tempa[3] = RoundKey[(i-1) * 4 + 3];
-    
-    if (i % 4 == 0) {
-        // This function rotates the 4 bytes in a word to the left once.
-        // [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
-        
-        // Rotate and sbox in one swoop
-        k = tempa[0];
-        tempa[0] = sbox[tempa[1]];
-        tempa[1] = sbox[tempa[2]];
-        tempa[2] = sbox[tempa[3]];
-        tempa[3] = sbox[k];
-
-        tempa[0] =  tempa[0] ^ Rcon[(i/4) - 1];
-    }
-
-    RoundKey[i * 4 + 0] = RoundKey[(i - 4) * 4 + 0] ^ tempa[0];
-    RoundKey[i * 4 + 1] = RoundKey[(i - 4) * 4 + 1] ^ tempa[1];
-    RoundKey[i * 4 + 2] = RoundKey[(i - 4) * 4 + 2] ^ tempa[2];
-    RoundKey[i * 4 + 3] = RoundKey[(i - 4) * 4 + 3] ^ tempa[3];
-  }
-}
-
-// This function adds the round key to state.
-// The round key is added to the state by an XOR function.
-static void AddRoundKey(uint8_t round) {
-  uint8_t i,j;
-  for(i=0;i<4;++i) {
-    for(j = 0; j < 4; ++j) {
-      (*state)[i][j] ^= RoundKey[round * 4 * 4 + i * 4 + j];
-    }
-  }
-}
+static uint8_t xtime(uint8_t x);
 
 // The SubBytes Function Substitutes the values in the
 // state matrix with values in an S-box.
@@ -250,121 +169,50 @@ static void MixColumns(void) {
   }
 }
 
-// Multiply is used to multiply numbers in the field GF(2^8)
-static uint8_t Multiply(uint8_t x, uint8_t y) {
-  return (((y & 1) * x) ^
-       ((y>>1 & 1) * xtime(x)) ^
-       ((y>>2 & 1) * xtime(xtime(x))) ^
-       ((y>>3 & 1) * xtime(xtime(xtime(x)))) ^
-       ((y>>4 & 1) * xtime(xtime(xtime(xtime(x))))));
-  }
-
-// MixColumns function mixes the columns of the state matrix.
-// The method used to multiply may be difficult to understand for the inexperienced.
-// Please use the references to gain more information.
-static void InvMixColumns(void) {
-  int i;
-  uint8_t a,b,c,d;
-  for(i=0;i<4;++i) { 
-    a = (*state)[i][0];
-    b = (*state)[i][1];
-    c = (*state)[i][2];
-    d = (*state)[i][3];
-
-    (*state)[i][0] = Multiply(a, 0x0e) ^ Multiply(b, 0x0b) ^ Multiply(c, 0x0d) ^ Multiply(d, 0x09);
-    (*state)[i][1] = Multiply(a, 0x09) ^ Multiply(b, 0x0e) ^ Multiply(c, 0x0b) ^ Multiply(d, 0x0d);
-    (*state)[i][2] = Multiply(a, 0x0d) ^ Multiply(b, 0x09) ^ Multiply(c, 0x0e) ^ Multiply(d, 0x0b);
-    (*state)[i][3] = Multiply(a, 0x0b) ^ Multiply(b, 0x0d) ^ Multiply(c, 0x09) ^ Multiply(d, 0x0e);
-  }
-}
-
-
-// The SubBytes Function Substitutes the values in the
-// state matrix with values in an S-box.
-static void InvSubBytes(void) {
-  uint8_t i,j;
-  for(i=0;i<4;++i) {
-    for(j=0;j<4;++j) {
-      (*state)[j][i] = rsbox[(*state)[j][i]];
-    }
-  }
-}
-
-static void InvShiftRows(void) {
-  uint8_t temp;
-
-  // Rotate first row 1 columns to right  
-  temp=(*state)[3][1];
-  (*state)[3][1]=(*state)[2][1];
-  (*state)[2][1]=(*state)[1][1];
-  (*state)[1][1]=(*state)[0][1];
-  (*state)[0][1]=temp;
-
-  // Rotate second row 2 columns to right 
-  temp=(*state)[0][2];
-  (*state)[0][2]=(*state)[2][2];
-  (*state)[2][2]=temp;
-
-  temp=(*state)[1][2];
-  (*state)[1][2]=(*state)[3][2];
-  (*state)[3][2]=temp;
-
-  // Rotate third row 3 columns to right
-  temp=(*state)[0][3];
-  (*state)[0][3]=(*state)[1][3];
-  (*state)[1][3]=(*state)[2][3];
-  (*state)[2][3]=(*state)[3][3];
-  (*state)[3][3]=temp;
-}
-
-
 // Cipher is the main function that encrypts the PlainText.
-static void Cipher(void) {
-  uint8_t round = 0;
+static void Cipher(uint8_t* key) {
+    uint8_t round = 0;
+    uint8_t i, j;
+    uint8_t rcon = 0x01;
+    
+    for(round = 0; round < 10; ++round) {
+        for(i=0;i<4;++i) {
+            for(j = 0; j < 4; ++j) {
+                (*state)[i][j] ^= key[i*4 + j];
+            }
+        }
 
-  // Add the First round key to the state before starting the rounds.
-  AddRoundKey(0); 
-  
-  // There will be Nr rounds.
-  // The first Nr-1 rounds are identical.
-  // These Nr-1 rounds are executed in the loop below.
-  for(round = 1; round < 10; ++round) {
-    SubBytes();
-    ShiftRows();
-    MixColumns();
-    AddRoundKey(round);
-  }
-  
-  // The last round is given below.
-  // The MixColumns function is not here in the last round.
-  SubBytes();
-  ShiftRows();
-  AddRoundKey(10);
-}
+        SubBytes();
+        ShiftRows();
 
-static void InvCipher(void) {
-  uint8_t round = 0;
+        if(round < 9) {
+            MixColumns();
+        }
 
-  // Add the First round key to the state before starting the rounds.
-  AddRoundKey(10); 
-
-  // There will be Nr rounds.
-  // The first Nr-1 rounds are identical.
-  // These Nr-1 rounds are executed in the loop below.
-  for(round=10-1; round > 0; round--) {
-    InvShiftRows();
-    InvSubBytes();
-    AddRoundKey(round);
-    InvMixColumns();
-  }
+        //key schedule
+        // compute the 16 next round key bytes
+        key[0] = sbox[key[13]] ^ key[0] ^ rcon;
+        key[1] = sbox[key[14]] ^ key[1];
+        key[2] = sbox[key[15]] ^ key[2];
+        key[3] = sbox[key[12]] ^ key[3];
+        for (i=4; i<16; i++) {
+            key[i] = key[i] ^ key[i-4];
+        }
+        // compute the next Rcon value
+        rcon = xtime(rcon);     
+    }
   
   // The last round is given below.
   // The MixColumns function is not here in the last round.
-  InvShiftRows();
-  InvSubBytes();
-  AddRoundKey(0);
+  //SubBytes();
+  //ShiftRows();
+  //AddRoundKey(10);
+    for(i=0;i<4;++i) {
+        for(j = 0; j < 4; ++j) {
+            (*state)[i][j] ^= key[i*4 + j];
+        }
+    }
 }
-
 static void BlockCopy(uint8_t* output, uint8_t* input) {
   uint8_t i;
   for (i = 0; i < 16; ++i) {
@@ -374,30 +222,13 @@ static void BlockCopy(uint8_t* output, uint8_t* input) {
 
 
 
-void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t* output) {
+void AES128_ECB_encrypt(uint8_t* input, uint8_t* key, uint8_t* output) {
   // Copy input to output, and work in-memory on output
   BlockCopy(output, input);
   state = (state_t*)output;
 
-  Key = key;
-  KeyExpansion();
-
-  // The next function call encrypts the PlainText with the Key using AES algorithm.
-  Cipher();
+  Cipher(key);
 }
-
-void AES128_ECB_decrypt(uint8_t* input, const uint8_t* key, uint8_t *output) {
-  // Copy input to output, and work in-memory on output
-  BlockCopy(output, input);
-  state = (state_t*)output;
-
-  // The KeyExpansion routine must be called before encryption.
-  Key = key;
-  KeyExpansion();
-
-  InvCipher();
-}
-
 
 static void test_encrypt_ecb(void)
 {
@@ -407,8 +238,6 @@ static void test_encrypt_ecb(void)
   uint8_t buffer[16];
 
   AES128_ECB_encrypt(in, key, buffer);
-
-  print("ECB decrypt: ", 13);
 
   if(0 == memcmp((char*) out, (char*) buffer, 16)) {
     print("SUCCESS!", 8);
